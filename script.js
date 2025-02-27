76fb686bc8b7521aec5eeff0cd678cbc6929f809
zaid2607 <148917889+zaid2607@users.noreply.github.com>
2025-02-26 21:49:54 -0500

    Add files via upload
    Added Javascript file

---
+++ script.js
@@ -0,0 +1,13 @@
+// Example JavaScript functionality
+
+// Handle form submissions
+function handleSubmit(event) {
+    event.preventDefault();
+    alert("Form submitted successfully!");
+  }
+  
+  // Quick action buttons (example)
+  function quickAction(action) {
+    alert(`${action} is currently being developed!`);
+  }
+  
